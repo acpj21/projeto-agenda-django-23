626c331829c8d329f5ecb7db96de068841f20290
Antonio Carlos Peron Júnior <antonio.peron@cmc.pr.gov.br>
2024-03-19 08:38:43 -0300

    Criando um formulário dinâmico com forms.ModelForm do Django (parte 2)

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -18,6 +18,7 @@ class ContactForm(forms.ModelForm):
 
     def clean(self):
         cleaned_data = self.cleaned_data
+        print(cleaned_data)
 
         self.add_error(
             'first_name',
